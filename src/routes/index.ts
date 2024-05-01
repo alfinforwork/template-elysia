3e794ad02c7a65b61a58487e8bb93e4482aa5600
alfin <alfinforwork@gmail.com>
2024-05-01 17:31:05 +0700

    update routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,10 @@ import Elysia from "elysia";
 import userRoutes from "./user.routes";
 
 const routes = (app: Elysia) => {
-  return app.use(userRoutes);
+  return (
+    app
+      // this comment used for prevent the linter because we just have one route
+      .group("/user", (app) => userRoutes(app))
+  );
 };
 export default routes;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,10 @@
 import Elysia, { t } from "elysia";
 import userController from "../controllers/userController";
 
-const userRoutes = (app: Elysia) => {
-  return app.group("/user", (groupApp) => {
-    return groupApp
-      .get("/user", userController.profile)
-      .post("/image", userController.changeImage);
-  });
+const userRoutes = (app: Elysia<"/user">) => {
+  return app
+    .get("/", userController.profile)
+    .post("/image", userController.changeImage);
 };
 
 export default userRoutes;
