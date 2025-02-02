d4eeba894cfced6228309491f1bd2b84ac07de1e
Mohamad Alfin Nahrowi <55651103+alfinforwork@users.noreply.github.com>
2025-02-03 00:58:49 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,20 @@ import response from "../core/response";
 const app = new Elysia();
 
 app
-  .onError(({ code }) => {
-    return response({ message: code });
-  })
-  .use(swagger({ path: "/docs", exclude: ["/docs", "/docs/json"] }))
-  .use(routes)
-  .get("/", (data) => {
-    return response({
-      data: {
-        hello: "world",
-      },
-    });
-  })
-  .listen(3000);
+.onError(({ code }) => {
+  return response({ message: code });
+})
+.use(swagger({ path: "/docs", exclude: ["/docs", "/docs/json"] }))
+.use(routes)
+.get("/", (data) => {
+  return response({
+    data: {
+      hello: "world",
+    },
+  });
+})
+.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
